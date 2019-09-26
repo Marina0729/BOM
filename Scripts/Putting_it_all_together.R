fb6dbda0f8946a673df34e48cc17b61c86697a95
Alexander <ale097@csiro.au>
2019-09-26 12:08:19 +1000

    Challenge 1 getting closer!

--- Scripts/Putting_it_all_together.R
+++ Scripts/Putting_it_all_together.R
@@ -16,11 +16,16 @@ BOM_stations
 #Challenge 1
 ##Question 1: For each station, how many days have a minimum temperature, a maximum temperature and a rainfall measurement recorded?
 
-seperated_temp <- BOM_data %>% 
-  separate(BOM_data, col = Temp_min_max, into = c("Temp_min", "Temp_Max"), sep ="/") %>% 
-  filter(Temp_min != "-", Temp_max != "-", Rainfall != 0) %>% 
-  group_by(station_number) %>% 
+ 
+BOM_data %>% 
+  separate(col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep ="/") %>% 
+  filter(Temp_min != "-") %>% 
+  filter(Temp_max !="-") %>% 
+  filter(Rainfall != 0) %>% 
+  group_by(Station_number) %>% 
   summarise(n = n())
+
+
 
 
   
