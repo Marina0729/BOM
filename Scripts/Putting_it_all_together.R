a1fa691b5eb4ae3bc9c5c0aebc018ebd5e408308
Alexander <ale097@csiro.au>
2019-09-28 19:43:44 +1000

    Comfortable that I now have the correct result for Challenge 1

--- Scripts/Putting_it_all_together.R
+++ Scripts/Putting_it_all_together.R
@@ -2,10 +2,10 @@
 library(tidyverse)
 
 read_csv("data/BOM_data.csv")
-BOM_data <- read_csv("data/BOM_data.csv")
+BOM_data <- read_csv("Data/BOM_data.csv")
 
 read_csv("data/BOM_stations.csv")
-BOM_stations <- read_csv("data/BOM_stations.csv")
+BOM_stations <- read_csv("Data/BOM_stations.csv")
 
 
 BOM_data
@@ -15,30 +15,96 @@ BOM_stations
 #Challenge 1
 #Question 1: For each station, how many days have a minimum temperature, a maximum temperature and a rainfall measurement recorded?
 
-Challenge1 <- 
+#Steps taken 
+#1--Separate Temp_min_max into two separate columns 
+#2--Remove rows in Temp_min and Temp_max containing "-" 
+#3--Remove rows in Rainfall containing "-"
+#4--Create a "Date" column since I don't know if more than one measurement was taken in one day
+#5--Don't need Solar_exposure column
+#6--Take existing tbl and reduce it to one row per station
+#7--summarise creating new colum "days_with_full_measurements" equals the
+#   number of unique values in the "Date" column
+
+Challenge1_0 <- 
   BOM_data %>% 
   separate(col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep ="/") %>% 
   filter(Temp_min != "-") %>% 
-  filter(Temp_max !="-") %>% 
-  filter(Rainfall != 0) %>%
-  select(-Solar_exposure) %>% 
-  mutate(date_identifier = Year + Month + Day) %>% 
+  filter(Temp_max != "-") %>% 
+  filter(Rainfall != "-") %>%
+  select(-Solar_exposure) %>%
+  unite(Year, Month, Day, col = "Date", sep = "-") %>%
   group_by(Station_number) %>% 
-  summarise(n_days = n_distinct(date_identifier))
+  summarise(days_with_full_measurements = n_distinct(Date))
 
-#Steps taken 
-#1. Separate Temp_min_max into two separate columns 
-#2. Remove rows in Temp_min and Temp_max containing "-" 
-#3. Remove rows in Rainfall containing 0
-#4. Create a new column to assign each day a unique number 
-#since I don't know if more than one measurement was taken in one
-#day
-#5. Take existing tbl and reduce it to per station
-#6. 
+write_csv(Challenge1_0,"Results/Challenge1_0.csv") 
 
+#Try it using group by Date instead of summarise number of distinct elements
 
+Challenge1_1 <- 
+  BOM_data %>% 
+  separate(col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep ="/") %>% 
+  filter(Temp_min != "-") %>% 
+  filter(Temp_max != "-") %>% 
+  filter(Rainfall != "-") %>%
+  select(-Solar_exposure) %>%
+  unite(Year, Month, Day, col = "Date", sep = "-") %>%
+  group_by(Date) %>% 
+  group_by(Station_number) %>% 
+  count(Station_number, Station_number)
 
+write_csv(Challenge1_1,"Results/Challenge1_1.csv") 
 
+#What would happen if I just reported the number of rows for each station
+#without making sure there was only one measurement taken per day? 
+
+Challenge1_2 <- 
+  BOM_data %>% 
+  separate(col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep ="/") %>% 
+  filter(Temp_min != "-") %>% 
+  filter(Temp_max != "-") %>% 
+  filter(Rainfall != "-") %>%
+  select(-Solar_exposure) %>% 
+  group_by(Station_number) %>% 
+  count(Station_number, Station_number)
+  
+#Same answer! This suggests there is one row per day. Let's test this using
+#group_by(Date) and see how many observations there are in the tibble.
+
+Check_number_unique_Dates <- 
+  BOM_data %>% 
+  separate(col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep ="/") %>% 
+  filter(Temp_min != "-") %>% 
+  filter(Temp_max != "-") %>% 
+  filter(Rainfall != "-") %>%
+  select(-Solar_exposure) %>%
+  unite(Year, Month, Day, col = "Date", sep = "-") %>%
+  group_by(Date)
+
+#There are 28340 obs. in this tibble
+
+Check_number_rows_after_filtering <- 
+  BOM_data %>% 
+  separate(col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep ="/") %>% 
+  filter(Temp_min != "-") %>% 
+  filter(Temp_max != "-") %>% 
+  filter(Rainfall != "-") %>%
+  group_by(Day)
+
+#There are 28340 obs. in this tibble
+
+#Lets check station 86344 is a tibble 1,020 obs.
+
+Check_station_86344 <- 
+  BOM_data %>% 
+  separate(col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep ="/") %>% 
+  filter(Temp_min != "-") %>% 
+  filter(Temp_max != "-") %>% 
+  filter(Rainfall != "-") %>%
+  select(-Solar_exposure) %>% 
+  filter(Station_number == 86344) %>% 
+  arrange(Year, Month, Day)
+
+#Yes
 
 #Challenge 2
 #Which month saw the lowest average daily temperature difference?
